1fd4649dcddd6e7bb304fba99076b584329226a5
FengyiQuan <quan.f@husky.neu.edu>
2020-05-22 01:07:52 -0400

    add table comp

--- src/components/CourseTableComponent.js
+++ src/components/CourseTableComponent.js
@@ -3,19 +3,21 @@ import CourseRowComponent from "./CourseRowComponent";
 
 export default class CourseTableComponent
     extends React.Component {
+
     render() {
         return (
             <div>
                 <h3>Course Table {this.props.courses.length}</h3>
-                <table>
+                <table className="table table-striped">
                     <thead>
                     <tr>
                         <th>Title</th>
-                        <th>Owner</th>
+                        <th>Owned by</th>
                         <th>Last Modified</th>
                         <th>
-                            <button>Sort</button>
-                            <button>Grid</button>
+                            <i onClick={() => this.props.setLayout('grid')}
+                               className="fa fa-th pr-2"/>
+                            <i className="fa fa-sort-alpha-asc"/>
                         </th>
                     </tr>
                     </thead>
