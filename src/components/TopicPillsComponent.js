aab7d37487948ee1766d38ddf23beddc0a7542b0
FengyiQuan <quan.f@husky.neu.edu>
2020-05-23 19:05:58 -0400

    Topic pills

--- src/components/TopicPillsComponent.js
+++ src/components/TopicPillsComponent.js
@@ -1,29 +1,26 @@
 import React from "react";
 
-export default class TopicPillsComponent extends React.Component {
-    render() {
-        return (
-            <div>
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Topic 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#">Topic 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Topic 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Topic 4</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">
-                            <i className="fa fa-plus"/>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const TopicPillsComponent = () =>
+    <div>
+        <ul className="nav nav-pills">
+            <li className="nav-item">
+                <a className="nav-link" href="#">Topic 1</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link active" href="#">Topic 2</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Topic 3</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Topic 4</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">
+                    <i className="fa fa-plus"/>
+                </a>
+            </li>
+        </ul>
+    </div>;
+
+export default TopicPillsComponent;
