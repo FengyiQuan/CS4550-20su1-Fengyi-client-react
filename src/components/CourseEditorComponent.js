c3c7378cff24cb52f36eb5fee9e3a06501a12ed8
FengyiQuan <quan.f@husky.neu.edu>
2020-06-08 06:02:31 -0400

    make editor stateful

--- src/components/CourseEditorComponent.js
+++ src/components/CourseEditorComponent.js
@@ -3,36 +3,48 @@ import {Link} from "react-router-dom";
 import ModuleListContainer from "../containers/ModuleListContainer";
 import LessonTabsContainer from "../containers/LessonTabsContainer";
 import TopicPillsContainer from "../containers/TopicPillsContainer";
+import CourseService from "../services/CourseService";
 
-// stateless component
-const CourseEditorComponent = ({match}) => {
+export default class CourseEditorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: ''
+        }
+    }
 
-    return (
-        <div>
-            {/*TODO:class name*/}
+    componentDidMount() {
+        CourseService.findCourseById(this.props.match.params.courseId)
+            .then(result =>
+                      this.setState({
+                                        title: result.title
+                                    }));
+    }
 
-            {match.params.courseId}
-
-            {/*{CourseService.findCourseById(match.params.courseId)}*/}
-            <div className="form-inline">
-                <Link to="/table/courses">
-                    <i className="fa fa-times fa-2x pr-2"/>
-                </Link>
-                <h2>Course Editor</h2>
-            </div>
-
-            <div className="row">
-                <div className="col-4">
-                    <ModuleListContainer {...match}/>
+    render() {
+        return (
+            <div>
+                <div className="form-inline">
+                    <Link to="/table/courses">
+                        <i className="fa fa-times fa-2x pr-2"/>
+                    </Link>
+                    <h2>Course Editor</h2>
                 </div>
-                <div className="col-8">
-                    <LessonTabsContainer {...match}/>
-                    <TopicPillsContainer {...match}/>
-                    <h3>Widget List</h3>
+
+                <h1>{this.state.title}</h1>
+                {this.props.match.params.courseId}
+
+                <div className="row">
+                    <div className="col-4">
+                        <ModuleListContainer {...this.props.match}/>
+                    </div>
+                    <div className="col-8">
+                        <LessonTabsContainer {...this.props.match}/>
+                        <TopicPillsContainer {...this.props.match}/>
+                        <h3>Widget List</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default CourseEditorComponent
