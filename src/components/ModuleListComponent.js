07eeaa8986d651c2bed24a7acef6a890c0e06797
FengyiQuan <quan.f@husky.neu.edu>
2020-06-08 04:42:10 -0400

    highlight module

--- src/components/ModuleListComponent.js
+++ src/components/ModuleListComponent.js
@@ -11,6 +11,61 @@ export default class ModuleListComponent extends React.Component {
         this.props.findModuleForCourse(this.props.params.courseId)
     }
 
+    renderEditing = (module) => (
+        <div>
+            {this.state.editingModule._id === module._id &&
+             <span>
+                 <input
+                     onChange={(e) => {
+                         const newTitle = e.target.value;
+                         this.setState(prevState => ({
+                             editingModule: {
+                                 ...prevState.editingModule,
+                                 title: newTitle
+                             }
+                         }))
+                     }
+                     }
+                     value={this.state.editingModule.title}
+                 />
+                 <button className="btn btn-danger"
+                         onClick={() => this.props.deleteModule(
+                             module._id)}>
+                     <i className="fa fa-trash"/>
+                 </button>
+                 <button
+                     className="btn btn-success"
+                     onClick={() => {
+                         this.props.updateModule(
+                             this.state.editingModule._id,
+                             this.state.editingModule);
+                         this.setState({editingModule: {}})
+                     }
+                     }>
+                     <i className="fa fa-check"/>
+                 </button>
+             </span>
+            }
+
+            {
+                this.state.editingModule._id !== module._id &&
+                <span>
+
+                    <Link
+                        to={`/editor/${this.props.params.courseId}/modules/${module._id}`}>
+                        {module.title}
+                    </Link>
+
+                    <button className="btn btn-primary"
+                            onClick={() => this.setState(
+                                {editingModule: module})}>
+                        <i className="fa fa-pencil"/>
+                    </button>
+                </span>
+            }
+
+        </div>);
+
     render() {
         return (
             <div>
@@ -19,69 +74,21 @@ export default class ModuleListComponent extends React.Component {
                         this.props.modules.map(
                             module =>
                                 <div key={module._id}>
-                                         {this.state.editingModule._id
-                                             === module._id &&
-                                             <li key={module._id}
-                                             className="list-group-item list-group-item-action list-group-item-primary">
-                                             <span>
-
-                                             <input
-                                             onChange={(e) => {
-                                                 const newTitle = e.target.value;
-                                                 this.setState(prevState => ({
-                                                     editingModule: {
-                                                         ...prevState.editingModule,
-                                                         title: newTitle
-                                                     }
-                                                 }))
-                                             }
-                                         }
-                                         value={this.state.editingModule.title}
-                                     />
-                                        <button className="btn btn-danger"
-                                                onClick={() => this.props.deleteModule(module._id)}>
-                                        <i className="fa fa-trash"/>
-                                        </button>
-                                        <button
-                                            className="btn btn-success"
-                                            onClick={() => {
-                                                this.props.updateModule(
-                                                    this.state.editingModule._id,
-                                                    this.state.editingModule);
-                                                this.setState({editingModule: {}})
-                                            }
-                                            }>
-                                        <i className="fa fa-check"/>
-                                            </button>
-                                     </span>
+                                    {this.props.params.moduleId === module._id &&
+                                     <li key={module._id}
+                                         className="list-group-item list-group-item-action list-group-item-primary">
+                                         {this.renderEditing(module)}
                                      </li>}
-
-
-                                    {
-                                        this.state.editingModule._id
-                                        !== module._id &&
-                                        <li key={module._id}
-                                            className="list-group-item list-group-item-action">
-                                        <span>
-
-                                        <Link
-                                            to={`/editor/${this.props.params.courseId}/modules/${module._id}`}>
-                                        {module.title}
-                                        </Link>
-
-                                        <button className="btn btn-primary"
-                                                onClick={() => this.setState(
-                                                    {editingModule: module})}>
-                                        <i className="fa fa-pencil"/>
-                                        </button>
-                                        </span>
-                                        </li>
+                                    {this.props.params.moduleId !== module._id &&
+                                     <li key={module._id}
+                                         className="list-group-item list-group-item-action">
+                                         {this.renderEditing(module)}
+                                     </li>
                                     }
-
                                 </div>
                         )}
                 </ul>
-                < input
+                <input
                     onChange={(event) =>
                         this.setState({
                                           newModuleTitle: event.target.value
